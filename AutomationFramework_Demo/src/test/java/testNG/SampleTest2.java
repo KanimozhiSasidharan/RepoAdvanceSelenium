a2ae6ca7bdcea7ca99c6d642e44f6dd5a79f9c96
Kanimozhi <Kanimozhi@DESKTOP-H8NBO3R>
2025-03-08 16:39:52 +0530

    Eng 2

---
+++ AutomationFramework_Demo/src/test/java/testNG/SampleTest2.java
@@ -0,0 +1,20 @@
+package testNG;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class SampleTest2 
+{
+	@Test
+	public void m1()
+	{
+		//Eng 2
+		 System.out.println("step1");
+		 System.out.println("step2");
+		 SoftAssert soft = new SoftAssert();
+		 soft.assertEquals(true, false);
+		 System.out.println("step3");
+		 System.out.println("step4");
+		 soft.assertAll();
+	}
+}
